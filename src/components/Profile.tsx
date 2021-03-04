084b0f31108fc9d2d857af1875a96cc3724f04dd
Lucas Duarte <lucasdrtaraujo@gmail.com>
2021-03-03 22:22:07 -0300

    Use level state of challenge context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengeContext } from '../contexts/challengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export default function Profile () {
+  const { level } = useContext(ChallengeContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/lucasdrta.png" alt="Profile"/>
@@ -8,7 +12,7 @@ export default function Profile () {
         <strong>Lucas Duarte</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
